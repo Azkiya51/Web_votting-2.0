00835ead60e9f0e96757fbb1762f757ae043c7b8
Zeck51 <azkiyafauzan@gmail.com>
2025-06-20 16:01:52 +0700

    Tambah File web

---
+++ public/script.js
@@ -0,0 +1,70 @@
+const socket = io();
+let currentRoomId = '';
+let participantName = '';
+
+function showCreate() {
+    document.getElementById('createRoomDiv').style.display = 'block';
+}
+function showJoin() {
+    document.getElementById('joinRoomDiv').style.display = 'block';
+}
+
+function createRoom() {
+    const roomName = document.getElementById('roomName').value;
+    socket.emit('createRoom', roomName);
+}
+
+function joinRoom() {
+    currentRoomId = document.getElementById('roomId').value;
+    participantName = document.getElementById('participantName').value;
+    socket.emit('joinRoom', { roomId: currentRoomId, name: participantName });
+}
+
+function sendArgument() {
+    const argument = document.getElementById('argument').value;
+    const logicType = document.getElementById('logicType').value;
+    socket.emit('sendArgument', { roomId: currentRoomId, argument, logicType });
+    document.getElementById('argument').value = '';
+}
+
+socket.on('roomCreated', (roomId) => {
+    alert(`Room berhasil dibuat dengan ID: ${roomId}`);
+    currentRoomId = roomId;
+    document.getElementById('createRoomDiv').style.display = 'none';
+    document.getElementById('roomArea').style.display = 'block';
+    document.getElementById('roomInfo').innerText = `Room ID: ${roomId}`;
+});
+
+socket.on('joinedRoom', (roomData) => {
+    document.getElementById('joinRoomDiv').style.display = 'none';
+    document.getElementById('roomArea').style.display = 'block';
+    document.getElementById('roomInfo').innerText = `Room ID: ${currentRoomId}`;
+    document.getElementById('participantsList').innerText = `Peserta: ${roomData.participants.join(', ')}`;
+});
+
+socket.on('participantsUpdate', (participants) => {
+    document.getElementById('participantsList').innerText = `Peserta: ${participants.join(', ')}`;
+});
+
+socket.on('newArgument', ({ argument, logicType }) => {
+    const div = document.getElementById('argumenArea');
+    div.innerHTML = `
+        <p>Argumen: ${argument} (${logicType})</p>
+        <button onclick="vote('agree', '${argument}')">Setuju</button>
+        <button onclick="vote('disagree', '${argument}')">Tidak Setuju</button>
+    `;
+});
+
+function vote(choice, argument) {
+    socket.emit('vote', { roomId: currentRoomId, argument, choice });
+}
+
+socket.on('voteUpdate', (voteData) => {
+    const total = voteData.agree + voteData.disagree;
+    const agreePct = ((voteData.agree / total) * 100).toFixed(2);
+    document.getElementById('argumenArea').innerHTML += `<p>✅ Setuju: ${agreePct}%</p>`;
+});
+
+socket.on('errorMsg', (msg) => {
+    alert(msg);
+});
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const rooms = {};
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('createRoom', (roomName) => {
+        const roomId = Math.random().toString(36).substring(2, 8).toUpperCase();
+        rooms[roomId] = {
+            name: roomName,
+            participants: [],
+            arguments: [],
+            votes: {}
+        };
+        socket.join(roomId);
+        socket.emit('roomCreated', roomId);
+    });
+
+    socket.on('joinRoom', ({ roomId, name }) => {
+        if (rooms[roomId]) {
+            rooms[roomId].participants.push(name);
+            socket.join(roomId);
+            socket.emit('joinedRoom', rooms[roomId]);
+            io.to(roomId).emit('participantsUpdate', rooms[roomId].participants);
+        } else {
+            socket.emit('errorMsg', 'Room ID tidak ditemukan.');
+        }
+    });
+
+    socket.on('sendArgument', ({ roomId, argument, logicType }) => {
+        rooms[roomId].arguments.push({ argument, logicType });
+        rooms[roomId].votes[argument] = { agree: 0, disagree: 0 };
+        io.to(roomId).emit('newArgument', { argument, logicType });
+    });
+
+    socket.on('vote', ({ roomId, argument, choice }) => {
+        if (rooms[roomId] && rooms[roomId].votes[argument]) {
+            rooms[roomId].votes[argument][choice]++;
+            io.to(roomId).emit('voteUpdate', rooms[roomId].votes[argument]);
+        }
+    });
+});
+
+http.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
